e799202e896c77f1040da1f1bf9a11d0427e7fb0
GitHub Action <action@github.com>
2022-12-07 08:44:29 +0000

    Template cleanup

--- src/main/kotlin/com/github/kkyeer/debuggertouml/MyBundle.kt
+++ src/main/kotlin/com/github/kkyeer/debuggertouml/MyBundle.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template
+package com.github.kkyeer.debuggertouml
 
 import com.intellij.DynamicBundle
 import org.jetbrains.annotations.NonNls
--- src/main/kotlin/com/github/kkyeer/debuggertouml/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/kkyeer/debuggertouml/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.kkyeer.debuggertouml.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.kkyeer.debuggertouml.MyBundle
 
 class MyApplicationService {
 
